f15ab9f448b1391d16e5fc8d3a7f1bbe45f25fcf
Jacob De La Rosa-Torres <jacobmatthew.delarosatorres@calbaptist.edu>
2016-04-05 11:10:40 -0700

    added to die class

--- src/yahtzeeGame/Die.java
+++ src/yahtzeeGame/Die.java
@@ -2,10 +2,17 @@ package yahtzeeGame;
 
 import java.util.Random;
 
+/**
+ * 
+ * @author Jacob De La Rosa-Torres, Peter Dunckel
+ *
+ */
+
 public class Die {
 
 	private int SIDES = 6;
 	private int rollValue;
+	private boolean rollEnabled;
 	
 	//Default Constructor
 	public Die(){};	
@@ -17,6 +24,14 @@ public class Die {
 	
 	public int getRollValue() {
 		return rollValue;
+	}
+	
+	public void setRollEnabled(boolean r){
+		rollEnabled = r;
+	}
+	
+	public boolean getRollEnabled(boolean r){
+		return rollEnabled;
 	}
 }
 
