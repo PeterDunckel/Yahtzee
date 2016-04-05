e6a78a24629589d238e966eb98f0ba794588d917
Jacob De La Rosa-Torres <jacobmatthew.delarosatorres@calbaptist.edu>
2016-04-05 10:51:51 -0700

    changed die class

--- src/yahtzeeGame/Die.java
+++ src/yahtzeeGame/Die.java
@@ -4,39 +4,19 @@ import java.util.Random;
 
 public class Die {
 
-	private int amtSides;
-	private int rollVal;
+	private int SIDES = 6;
+	private int rollValue;
 	
 	//Default Constructor
 	public Die(){};	
-	
-	//Set sides constructor
-	public Die(int amtS) {
-		setAmtSides(amtS);
-	}
 
-	//Roll a die, randomly roll an amount between the min and max of die
 	public void rollDie() {
 		Random rand = new Random();
-		if(amtSides > 1)
-			rollVal = rand.nextInt(amtSides) + 1;
+		rollValue = rand.nextInt(SIDES) + 1;
 	}
 	
-	//Get and set functions	
-	public int getAmtSides() {
-		return amtSides;
+	public int getRollValue() {
+		return rollValue;
 	}
-	
-	public int getRollVal() {
-		return rollVal;
-	}
-	
-	public void setAmtSides(int amtSides) {
-		if(amtSides < 2)
-			this.amtSides = 2;
-		else
-			this.amtSides = amtSides;
-	}	
-
-	
 }
+
