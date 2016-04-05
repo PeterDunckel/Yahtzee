04a420398189a674d00956e32264cdb54730320b
PeterDunckel <peterdunckel94@yahoo.com>
2016-04-05 10:45:11 -0700

    Added Die implementation

--- src/yahtzeeGame/Die.java
+++ src/yahtzeeGame/Die.java
@@ -1,8 +1,42 @@
 package yahtzeeGame;
 
-public class Die {
-	
-	
-	
+import java.util.Random;
 
+public class Die {
+
+	private int amtSides;
+	private int rollVal;
+	
+	//Default Constructor
+	public Die(){};	
+	
+	//Set sides constructor
+	public Die(int amtS) {
+		setAmtSides(amtS);
+	}
+
+	//Roll a die, randomly roll an amount between the min and max of die
+	public void rollDie() {
+		Random rand = new Random();
+		if(amtSides > 1)
+			rollVal = rand.nextInt(amtSides) + 1;
+	}
+	
+	//Get and set functions	
+	public int getAmtSides() {
+		return amtSides;
+	}
+	
+	public int getRollVal() {
+		return rollVal;
+	}
+	
+	public void setAmtSides(int amtSides) {
+		if(amtSides < 2)
+			this.amtSides = 2;
+		else
+			this.amtSides = amtSides;
+	}	
+
+	
 }
