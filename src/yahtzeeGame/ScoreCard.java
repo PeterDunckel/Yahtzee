68fb34618eea530fddb7b3d5302025549311a987
Jacob De La Rosa-Torres <jacobmatthew.delarosatorres@calbaptist.edu>
2016-04-06 22:27:31 -0700

    finish scorecard logic

--- src/yahtzeeGame/ScoreCard.java
+++ src/yahtzeeGame/ScoreCard.java
@@ -10,7 +10,9 @@ public class ScoreCard {
 	private int lowerScore;
 	private int totalScore;
 	
-	public ScoreCard(){}
+	public ScoreCard(){
+		lowerSection[6] = -1;
+	}
 	
 	public void addScoreToUpperSection(int score, int pos){
 		upperSection[pos] = score;
@@ -103,20 +105,59 @@ public class ScoreCard {
 	
 	public boolean isfullHouse(Die[] dice){
 		
+		int valueA = dice[0].getRollValue();
+		int valueB = 0;
+		int a = 0, b = 0;
 		
-		
-		return false;
+		for(int i = 1; i < 5; i++){
+			
+			if(valueB == 0 && dice[i].getRollValue() != valueA){
+				valueB = dice[i].getRollValue();
+			} else if(dice[i].getRollValue() == valueA){
+				a++;
+			} else if(dice[i].getRollValue() == valueB){
+				b++;
+			}
+			
+		}
+		return (a == 1 && b == 2) || (a == 2 && b == 1);
 	}
 	
 	// CHECK FOR FULL HOUSE
 	
 	public boolean isStraight(Die[] dice, int n){
 			
-			
-			
+		int one = 0, two = 0, three = 0, four = 0, five = 0, six = 0;
+		
+		for (Die d : dice){
+			if(d.getRollValue() == 1)
+				one++;
+			else if(d.getRollValue() == 2)
+				two++;
+			else if(d.getRollValue() == 3)
+				three++;
+			else if(d.getRollValue() == 4)
+				four++;
+			else if(d.getRollValue() == 5)
+				five++;
+			else if(d.getRollValue() == 6)
+				six++;
+		}
+		
+		if(n == 4){
+			return (one >= 1 && two >= 1 && three >= 1 && four >= 1 ||
+					two >= 1 && three >= 1 && four >= 1 && five >= 1 ||
+					three >= 1 && four >= 1 && five >= 1 && six >= 1);
+		} else if(n == 5){
+			return (one >= 1 && two >= 1 && three >= 1 && four >= 1 && five >=1 ||
+					two >= 1 && three >= 1 && four >= 1 && five >= 1 && six >= 1);
+		}
+		
 		return false;
 	}
 	
-	
+	public boolean chance(){
+		return lowerSection[6] == -1;
+	}
 	
 }
