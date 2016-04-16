87b6e9e29967cc186a9370cb6cdb6f206c7db808
Jacob De La Rosa-Torres <jacobmatthew.delarosatorres@calbaptist.edu>
2016-04-15 21:52:44 -0700

    added Of a Kinder

---
+++ src/yahtzeeGame/OfAKinderStrategy.java
@@ -0,0 +1,20 @@
+package yahtzeeGame;
+
+public class OfAKinderStrategy implements Strategy {
+
+	@Override
+	public int[] pickDiceToRoll(Die[] dice) {
+		
+		
+		
+		
+		return null;
+	}
+
+	@Override
+	public int pickCategory(Die[] dice) {
+		
+		return 0;
+	}
+
+}
